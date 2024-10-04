c52297bef3e851a891adfd863300a5b731ee4787
muhsina361 <muhsinamuhsina81@gmail.com>
2024-10-04 11:19:46 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,11 @@
+num1=int(input("enter a number"))
+num2=int(input("enter a number"))
+sum=num1+num2
+print(sum)
+substraction=num1-num2
+print(substraction)
+multiplication=num1*num2
+print(multiplication)
+division=num1/num2
+print(division)
+

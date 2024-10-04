f88cc40d6e51b259c6c81a1b76632df09c43fb93
muhsina361 <muhsinamuhsina81@gmail.com>
2024-10-04 11:20:11 +0530

    Add files via upload

---
+++ rct.py
@@ -0,0 +1,14 @@
+rad=int(input("enter the radius"))
+area=3.14*rad*rad
+print(area )
+"""area of rectangle"""
+l=int(input("length"))
+b=int(input("breadth"))
+rectangle =l*b
+print (rectangle)
+"""area of triangle"""
+b=int(input("base"))
+h=int(input("height"))
+triangle =0.5*b*h
+print(triangle)
+

b0f1e4cd928079483f5ba4a01f77ed4a07db9232
muhsina361 <muhsinamuhsina81@gmail.com>
2024-10-04 11:20:19 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,11 @@
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+
+print("before swapping")
+print("value of x:",x,"and y:",y)
+
+x=x+y
+y=x-y
+x=x-y
+print("after swaping")
+print("value of x:",x,"and y:",y)

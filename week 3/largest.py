3eadc895b3b384d99a7543d4546d55ba2f160058
muhsina361 <muhsinamuhsina81@gmail.com>
2024-10-09 11:14:44 +0530

    Add files via upload

---
+++ week 3/largest.py
@@ -0,0 +1,9 @@
+n1=int(input("enter number"))
+n2=int(input(" enter a number"))
+n3=int(input("enter a number"))
+if(n1>n2) and (n1>n3):
+    print(n1,"laargest")
+elif(n2>n3) and (n2>n1):
+    print(n2,largest)
+else:
+    print(n3,"largest")
